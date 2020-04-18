2acfb8d60145b406aeb8f633be7ee87f088727df
Nat! <nat@mulle-kybernetik.com>
2020-04-18 23:17:44 +0200

    * new mulle-sde project structure

--- RELEASENOTES.md
+++ RELEASENOTES.md
@@ -1,3 +1,7 @@
+### 0.17.2
+
+* new mulle-sde project structure
+
 ## 0.17.0
 
 * move HTTP code to MulleObjCHTTPFoundation
--- cmake/share/StandaloneC.cmake
+++ cmake/share/StandaloneC.cmake
@@ -31,7 +31,7 @@ if( STANDALONE)
    endif()
 
    if( NOT STANDALONE_DEFINITIONS)
-      set( STANDALONE_DEFINITIONS ${mulle_curl_DEFINITIONS})
+      set( STANDALONE_DEFINITIONS ${MULLE_CURL_DEFINITIONS})
    endif()
 
    #
